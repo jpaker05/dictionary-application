96fd5ebd7d508cfe35eac1babf08c3a27974dd50
jpaker05 <110342564+jpaker05@users.noreply.github.com>
2023-02-19 08:45:42 -0600

    added text and audio phonetics

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,17 +5,15 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (defintion, index) {
+      {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <p>
-              <strong>Definition:</strong>
-              {defintion.definition}
+              <strong>Definition:</strong> {definition.definition}
               <br />
               <strong>Example:</strong>
-              <em> {defintion.example} </em>
-              <br />
-              <Synonyms synonyms={defintion.synonyms} />
+              <em>{definition.example}</em>
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
