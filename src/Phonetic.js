87933c3b6a45ea70ba3b3672d208a46adc985bf6
jpaker05 <110342564+jpaker05@users.noreply.github.com>
2023-02-19 11:27:11 -0600

    began styling components

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,12 +1,14 @@
 import React from "react";
+import "./Phonetic.css";
 
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        🔊 <br />
+        🔊
       </a>
-      {props.phonetic.text}
+      <br />
+      <span className="text">{props.phonetic.text}</span>
     </div>
   );
 }
